f4375e41f42e9a34ae072cfa17205f148236c554
aragaer <aragaer@gmail.com>
2015-03-07 11:47:46 +0300

    High-level test draft for simple API

--- features/steps/steps.py
+++ features/steps/steps.py
@@ -12,3 +12,18 @@ def verify_today_file(context, directory):
     """
     {text}
     """'''.format(path=filepath, text=context.text))
+
+
+@given('I have the following memos for date {date}')
+def insert_memos_for_date(context, date):
+    raise NotImplementedError('STEP: Given I have the following memos for date {date}')
+
+
+@when('I access the resource \'{path}\'')
+def access_resource(context, path):
+    raise NotImplementedError('STEP: When I access the resource \'{path}\'')
+
+
+@then('I get the following list of memos')
+def check_memos_result(context):
+    raise NotImplementedError('STEP: Then I get the following list of memos')
